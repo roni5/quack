73225813f6cae6fdbee5b0bc9bcf20f3e4bb0239
“Roni” <roniemail1@gmail.com>
2018-12-03 23:50:04 +0000

    first

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,17 +2,21 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import Socket from '../socket'
+
 
 const App = () => (
   <div>
     <header>
       <Link to="/">Home</Link>
       <Link to="/about-us">About</Link>
+      <Link to="/socket">Socket</Link>
     </header>
 
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
+      <Route exact path="/socket" component={Socket} />
     </main>
   </div>
 )
